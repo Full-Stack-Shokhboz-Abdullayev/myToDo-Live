895a1edd1962a0f4eb393c06aa7aea3fd6a9b83f
Shokhboz Abdullaev <abdullaevadiana27@gmail.com>
2020-06-22 17:28:17 +0300

    successful

--- todo-new/src/components/fetcher/task-fetcher.js
+++ todo-new/src/components/fetcher/task-fetcher.js
@@ -19,7 +19,7 @@ export class CSRF {
 }
 
 export default class FetcherTasks {
-	_apiBase = "http://127.0.0.1:8000/api";
+	_apiBase = "https://shokhboz-to-do-app.herokuapp.com/api";
 
 	async fetchAllTasks() {
 		const tasksUrl = await fetch(`${this._apiBase}/all-tasks/`);
